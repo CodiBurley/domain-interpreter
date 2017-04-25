f9d7e000897515cced0f8bfb82c2239ed7b39e67
Codi Burley <idocyelrub@gmail.com>
2017-04-24 23:09:49 -0700

    working on interperter, still needs lots of teting and what not

--- src/interpreter.hs
+++ src/interpreter.hs
@@ -3,15 +3,34 @@ module Interpreter
 ) where
 import Structure
 
-type Env = [(String, Expression)]
+type Env = [(String, Maybe Expression)]
 
-interpretExpression :: Expression -> Env -> (Maybe Expression, Env)
+interpExpressionEnv :: Expression -> Env -> (Maybe Expression, Env)
+interpExpressionEnv exp@(Declare (BindValue s _)) e = (interped, (s, interped):e)
+  where interped = interpExpression exp e
+interpExpressionEnv exp e = (interpExpression exp e, e) 
 
-interpretExpression (ValueOf (Value s)) e = (envLookup e s, e)
+interpExpression :: Expression -> Env -> Maybe Expression
 
-interpretExpression (Declare (BindValue s exp)) e = (Just exp, (s, exp):e)
-  
+interpExpression (ValueOf (Value s)) e = envLookup e s
+
+interpExpression (Declare (BindValue s exp)) e = Just exp
+
+-- TODO binding to functions?
+--interpExpression (Declare (BindFunction (Function s exp))) e
+
+interpExpression (ArithmeticExp (Variable b)) e = interpExpression (ValueOf b) e
+
+interpExpression (ArithmeticExp (Evaluate fc)) e = interpFuncCall fc e
+
+
+interpFuncCall :: FunctionCall -> Env -> Maybe Expression;
+interpFuncCall (FunctionCall fe pe) e =
+  case funcExp of
+    Just (FunctionExp (Function s exp)) -> interpExpression exp e
+    _                             -> Nothing
+  where funcExp = interpExpression fe e
 
 envLookup :: Env -> String -> Maybe Expression
 envLookup [] _ = Nothing
-envLookup ((s, x):e) val = if s == val then Just x else (envLookup e val)
+envLookup ((s, x):e) val = if s == val then x else (envLookup e val)
