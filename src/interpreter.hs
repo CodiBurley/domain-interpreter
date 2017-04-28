b0720c14cef44e36c024d3327adfaab00724edb9
Codi Burley <idocyelrub@gmail.com>
2017-04-27 23:11:52 -0700

    added function with no parameters interp, int literal interp, and tested Arithmetic function evaluation with no parameter functions

--- src/interpreter.hs
+++ src/interpreter.hs
@@ -5,10 +5,26 @@ import Structure
 
 type Env = [(String, Maybe Expression)]
 
+-- Some interpretations have effects on the environment that
+-- they were interpreted in. For example:
+--   myvar is 6;
+-- this expression evaluates to the integer constant 6, but it
+-- also adds myvar to the environment is evaluated in.
+-- 
+-- interpExpressionEnv gives the interpretation of an expression, but
+-- also the environment that is resulting from the inpretation. You 
+-- can fold a script of expressions with this function to interpret the
+-- script
 interpExpressionEnv :: Expression -> Env -> (Maybe Expression, Env)
+-- Interpretations that effect environment
 interpExpressionEnv exp@(Declare (BindValue s _)) e = (interped, (s, interped):e)
   where interped = interpExpression exp e
+interpExpressionEnv exp@(FunctionExp (Function s _)) e = (interped, (s, interped):e)
+  where interped = interpExpression exp e
+
+-- Implementations that have no effect on environment
 interpExpressionEnv exp e = (interpExpression exp e, e) 
+
 
 interpExpression :: Expression -> Env -> Maybe Expression
 
@@ -19,6 +35,10 @@ interpExpression (Declare (BindValue s exp)) e = Just exp
 -- TODO binding to functions?
 --interpExpression (Declare (BindFunction (Function s exp))) e
 
+interpExpression exp@(FunctionExp fe) e = Just $ exp
+
+interpExpression exp@(ArithmeticExp (IntLiteral i)) e = Just $ exp
+
 interpExpression (ArithmeticExp (Variable b)) e = interpExpression (ValueOf b) e
 
 interpExpression (ArithmeticExp (Evaluate fc)) e = interpFuncCall fc e
@@ -28,9 +48,23 @@ interpFuncCall :: FunctionCall -> Env -> Maybe Expression;
 interpFuncCall (FunctionCall fe pe) e =
   case funcExp of
     Just (FunctionExp (Function s exp)) -> interpExpression exp e
-    _                             -> Nothing
+    _                                   -> Nothing
   where funcExp = interpExpression fe e
 
 envLookup :: Env -> String -> Maybe Expression
 envLookup [] _ = Nothing
 envLookup ((s, x):e) val = if s == val then x else (envLookup e val)
+
+
+test1 = interpExpression
+  (ArithmeticExp
+    (Evaluate
+      (FunctionCall
+        (FunctionExp
+          (Function
+            "doesntMatter"
+            (ArithmeticExp 
+              (IntLiteral 42))))
+        (ArithmeticExp
+          (IntLiteral 1)))))
+  []
